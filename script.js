bf87987b196035d68f194a679c54e5d43dd35fd5
Venkata Ramesh K <kvram2511@gmail.com>
2025-09-29 17:43:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const taskForm = document.getElementById('task-form');
+const taskTitleInput = document.getElementById('task-title');
+const taskDateInput = document.getElementById('task-date');
+const taskTimeInput = document.getElementById('task-time');
+const taskPrioritySelect = document.getElementById('task-priority');
+const taskList = document.getElementById('task-list');
+const progressBar = document.getElementById('progress-bar');
+const progressText = document.getElementById('progress-text');
 
+let tasks = [];
